60b8dfc1b8387f02f7718e86fbe14ca043d6d3da
Fedor <fs.golubkov@gmail.com>
2020-04-13 01:03:06 +0300

    1

--- src/Reader.php
+++ src/Reader.php
@@ -7,13 +7,13 @@ function read($filepath)
     if (!file_exists($filepath)) {
         throw new \Exception("File '$filepath' does not exists");
     }
-    
+
     return file_get_contents($filepath);
 }
 
 function getFileFormat($filepath)
 {
     $fileExtension = pathinfo($filepath, PATHINFO_EXTENSION);
-    
+
     return $fileExtension == 'yaml' ? 'yml' : $fileExtension;
 }
