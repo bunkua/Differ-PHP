d61aa2bae76cfc42006c793351d489450e57675d
Fedor <fs.golubkov@gmail.com>
2020-04-22 00:18:07 +0300

    Tests refactoring

--- tests/GendiffTest.php
+++ tests/GendiffTest.php
@@ -6,18 +6,16 @@ use PHPUnit\Framework\TestCase;
 
 class GendiffTest extends TestCase
 {
+    private $fixturesPath = "tests/fixtures/";
+    
     /**
     * @dataProvider dataProvider
     */
     public function testgenDiff($inputFormat, $outputFormat)
     {
-        $fixturesPath = "tests/fixtures/";
-        $dataPath = "{$fixturesPath}{$inputFormat}/";
-
-        $beforePath = "{$dataPath}/before.{$inputFormat}";
-        $afterPath = "{$dataPath}/after.{$inputFormat}";
-        
-        $resultPath = "{$fixturesPath}/{$outputFormat}.txt";
+        $beforePath = $this->getPath($inputFormat, 'before');
+        $afterPath = $this->getPath($inputFormat, 'after');
+        $resultPath = $this->getPath($outputFormat, 'result');
         $expected = file_get_contents($resultPath);
 
         $this->assertEquals($expected, \Differ\Differ\genDiff($beforePath, $afterPath, $outputFormat));
@@ -33,5 +31,16 @@ class GendiffTest extends TestCase
             ['yaml', 'plain'],
             ['yaml', 'json']
         ];
+    }
+
+    private function getPath($format, $fileName)
+    {
+        $fixturesPath = $this->fixturesPath;
+
+        if ($fileName === 'result') {
+            return "{$fixturesPath}{$format}.txt";
+        }
+
+        return "{$fixturesPath}/{$format}/{$fileName}.{$format}";
     }
 }
