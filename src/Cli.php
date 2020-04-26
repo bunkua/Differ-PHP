00df0f8b43ae30f3c2fae0557a8385876cb54b7c
bunkua <1507ksap1986ksap>
2020-04-26 03:28:51 +0300

    Refactoring

--- src/Cli.php
+++ src/Cli.php
@@ -2,13 +2,15 @@
 
 namespace Differ\Cli;
 
+use Docopt;
+
 use function Differ\Differ\genDiff;
 
 function run()
 {
     $doc = file_get_contents(__DIR__ . '/helpfile.docopt');
 
-    $args = \Docopt::handle($doc, array('version' => 'dev'));
+    $args = Docopt::handle($doc, array('version' => 'dev'));
     $pathToFile1 = $args->args['<firstFile>'];
     $pathToFile2 = $args->args['<secondFile>'];
     $format = $args->args["--format"];
